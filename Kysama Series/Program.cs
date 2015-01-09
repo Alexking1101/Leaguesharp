81825c566db766d1307d19f67e731d084338a3e6
Kysama <Alex.zwiebel@web.de>
2015-01-09 19:28:18 +0100

    Kysama Series

---
+++ Kysama Series/Program.cs
@@ -0,0 +1,47 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+#endregion
+
+namespace KS
+{
+    class Program
+    {
+        public static string ChampionName;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            ChampionName = ObjectManager.Player.BaseSkinName;
+
+            switch (ChampionName)
+            {
+                case "Blitzcrank":
+                    new Blitzcrank();
+                    break;
+
+                case "Akali":
+                    new Akali();
+                    break;
+
+                case "XinZhao":
+                    new XinZhao();
+                    break;
+
+                default:
+                    Game.PrintChat("This champion is not supported");
+                    break;
+            }
+        }
+    }
+}
